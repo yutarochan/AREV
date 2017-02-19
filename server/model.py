810047b698eb4883dc2f560531fbf1b259ae3a4e
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-02-19 18:45:43 -0500

    Add Initial Data Transmission Modules

--- server/model.py
+++ server/model.py
@@ -2,8 +2,34 @@
 AREV - Model Management Class
 Author: Yuya Jeremy Ong (yuyajeremyong@gmail.com)
 '''
+import gensim
 import logging
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 class Model:
     def __init__(self, model_path):
         print 'Initializing Model: ' + model_path
+        self.model_path = model_path
+        self.model = gensim.models.Word2Vec.load(model_path)
+
+        # Load Corpus
+        print 'Loading Corpus...'
+        self.vectors = []
+        for i in self.model.index2word:
+            self.vectors.append(self.model[i])
+        print 'Done'
+
+    def get3DModel(self):
+        print 'Perform PCA for 3 Components'
+        pca = PCA(n_components=3, whiten=True)
+        return pca.fit(self.vectors).transform(self.vectors)
+
+    def get2DModel(self):
+        print 'Perform PCA for 2 Components'
+        pca = PCA(n_components=2, whiten=True)
+        return pca.fit(self.vectors).transform(self.vectors)
+
+if __name__ == '__main__':
+    m = Model('../data/sample.model')
--- server/server.py
+++ server/server.py
@@ -11,9 +11,14 @@ import tornado.httpserver
 import tornado.ioloop
 from tornado.options import define, options, parse_command_line
 
+from model import Model
+
 # Logging and Command Line Arguments
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s')
 define("port", default=9892, help="Initialize socket server on the given port", type=int)
+
+# Load Model from File
+m = Model('../data/sample.model')
 
 # WebSocket Event Handler
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
@@ -23,15 +28,24 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         logging.info('[INFO]: WebSocket Connection Initialized')
 
-        # Initialize Handshake
-
     def on_message(self, message):
         try:
-            print message
             packet = json.loads(message)
-            print packet
-        except:
+            output = dict()
+
+            if packet['cmd'] == 'fetchData3D':
+                output['resp'] = 'fetchData3D'
+                output['data'] = m.get3DModel().tolist()
+                self.write_message(str(json.dumps(output)))
+
+            if packet['cmd'] == 'fetchData2D':
+                output['resp'] = 'fetchData2D'
+                output['data'] = m.get2DModel().tolist()
+                self.write_message(str(json.dumps(output)))
+
+        except Exception as err:
             print 'error handling packet'
+            print err
             # return self.write_message(self.create_packet('status', "Error parsing incoming packet."))
 
     def on_close(self):
